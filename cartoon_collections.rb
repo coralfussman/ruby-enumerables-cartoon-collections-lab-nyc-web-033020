8af78d6bca8d0802d23ac9b0d5974503a7b6bd7f
Coral Fussman <coralfussman@gmail.com>
2020-03-24 01:38:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,9 @@ dwarves.each_with_index do | item, index |
 end
 
 def summon_captain_planet(planeteer_calls)
-
+  planeteer_calls.collect do |call|
+    puts "#{call} + !"
+   end
 end
 
 def long_planeteer_calls# code an argument here
